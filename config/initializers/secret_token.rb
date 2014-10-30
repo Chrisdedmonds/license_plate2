1bca1d3d5bc2e0bb7db6841e89c742a215c974cb
chrisdedmonds <chris.edmonds128@gmail.com>
2014-10-30 00:04:38 -0400

    initial check-in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LicensePlate2::Application.config.secret_key_base = 'fb26516b20b76488b470e5fccbfe8b1f85e420de5ab3fa7433b8e570c4398b7f643163fd98f5e82b3028877e2da31f70797681094f59e6a54a1d31672fd56201'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LicensePlate2::Application.config.session_store :cookie_store, key: '_license_plate2_session'
